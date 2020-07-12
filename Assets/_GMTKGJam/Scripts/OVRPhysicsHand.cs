3ee2a59d5a210a713bb125686a0778122d2e6225
Vincent Wing <vincentwing00@gmail.com>
2020-07-11 17:57:52 -0700

    hand follows other hand in a Gatball-like manner. Still need to get rotations right.

---
+++ Assets/_GMTKGJam/Scripts/OVRPhysicsHand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRPhysicsHand : MonoBehaviour
+{
+    public OVRCustomSkeleton skeletonToCopy;
+    public Transform[] theirJoints = new Transform[0];
+    public Rigidbody[] ourJoints = new Rigidbody[0];
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Assert(theirJoints.Length == ourJoints.Length, "Our joints should be the same size as their joints!");
+        Debug.Log(skeletonToCopy.Bones.Count + " " + skeletonToCopy.CustomBones.Count);
+        //for (int i = 0; i < ourJoints.Length; ++i)
+        //    ourJoints[i].transform.SetParent(rb.transform);
+    }
+
+    private void Update()
+    {
+        rb.velocity = (skeletonToCopy.transform.position - transform.position) / Time.fixedDeltaTime;
+        rb.angularVelocity = Vector3.zero;
+        //rb.MovePosition(skeletonToCopy.transform.position);
+        //rb.MoveRotation(skeletonToCopy.transform.rotation);
+        rb.rotation = (skeletonToCopy.transform.rotation);
+        for (int i = 0; i < theirJoints.Length; ++i)
+        {
+            ourJoints[i].velocity = (theirJoints[i].transform.position - ourJoints[i].transform.position) / Time.fixedDeltaTime;
+            ourJoints[i].angularVelocity = Vector3.zero;
+            //ourJoints[i].MovePosition(theirJoints[i].position);
+            ourJoints[i].MoveRotation(theirJoints[i].rotation);
+            ourJoints[i].rotation = (theirJoints[i].rotation);
+        }
+    }
+}
